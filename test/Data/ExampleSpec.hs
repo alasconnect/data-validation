d63bc39d1c33063d1753bdc43a83edb4d897d07d
Sam Erie <sbe@alasconnect.com>
2020-12-17 11:33:40 -0900

    changed examples from executable to part of test-spec. added shell.nix to help with local testing.

--- test/Data/ExampleSpec.hs
+++ test/Data/ExampleSpec.hs
@@ -1,4 +1,4 @@
-module Examples.Data.ExampleSpec where
+module Data.ExampleSpec where
 
 ------------------------------------------------------------------------------------------------------------------------
 import Prelude
@@ -12,12 +12,12 @@ import Examples.Data.Primitives
 spec :: Spec
 spec = parallel $ do
 
-  describe "Example Type Validation" $ do
-    it "Valid Complex Type" $ do
+  describe "Example ComplexTypes Validation" $ do
+    it "Valid UserVM produces valid Proof" $ do
       let validForm = UserVM (Just "Hugh Mann") (Just "person@earth.com") Nothing (Just Email) Nothing
           userProof = validate validForm
       userProof `shouldSatisfy` isValid
-    it "Invalid Complex Type" $ do
+    it "Invalid UserVM produces invalid Proof" $ do
       let invalidForm = UserVM (Just "broken") Nothing (Just "123") (Just Email) Nothing
           userFailure = validate invalidForm
       userFailure `shouldSatisfy` isInvalid
